63b88d8220a5a6a2b1d9ae1a23c20b23d5bf4b87
jooweeah <juliainorbit@gmail.com>
2026-02-26 21:02:32 -0500

    Created React exercise application

---
+++ exercise-app/src/App.js
@@ -0,0 +1,48 @@
+import { Component } from 'react';
+import './App.css';
+import DurationExercise from './components/DurationExercise';
+import RepetitionExercise from './components/RepetitionExercise';
+
+const exercises = [
+  { name: 'Push Ups', type: 'duration' },
+  { name: 'Running', type: 'repetition' },
+  { name: 'Plank', type: 'repetition' },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentExercise: null };
+  }
+
+  render() {
+    const { currentExercise } = this.state;
+
+    if (currentExercise === null) {
+      return (
+        <div className="screen">
+          <h1 className="heading">Exercises</h1>
+          {exercises.map(ex => (
+            <div key={ex.name} className="exercise-card">
+              <span className="card-name">{ex.name}</span>
+              <button
+                className="btn-black"
+                onClick={() => this.setState({ currentExercise: ex })}
+              >
+                Start
+              </button>
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    if (currentExercise.type === 'duration') {
+      return <DurationExercise name={currentExercise.name} />;
+    }
+
+    return <RepetitionExercise name={currentExercise.name} />;
+  }
+}
+
+export default App;
